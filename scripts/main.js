727ed1d62807c7d84821af2102b9ba9d9a78c729
Fogel Egor <fogel_92@bk.ru>
2022-07-12 21:29:16 +0700

    feat: adding tasks

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,45 @@ function addTask() {
   const cancelBtn = document.querySelector('.cancel__item-btn');
   const textarea = document.querySelector('.textarea');
   const form = document.querySelector('.form');
+
+  let value;
+  btn.addEventListener('click', () => {
+    form.style.display = 'block';
+    btn.style.display = 'none';
+    addBtn.style.display = 'none';
+
+    textarea.addEventListener('input', (evt) => {
+      value = evt.target.value;
+      if (value) {
+        addBtn.style.display = 'block';
+      } else {
+        addBtn.style.display = 'none';
+      }
+    });
+  });
+
+  cancelBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    textarea.value = '';
+    value = '';
+    form.style.display = 'none';
+    btn.style.display = 'flex';
+  });
+
+  addBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const newItem = document.createElement('div');
+    newItem.classList.add('list__item');
+    newItem.draggable = true;
+    newItem.textContent = value;
+    lists[0].append(newItem);
+
+    form.style.display = 'none';
+    textarea.value = '';
+    value = '';
+    form.style.display = 'none';
+    btn.style.display = 'flex';
+  });
 }
+
+addTask();
