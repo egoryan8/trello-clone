65751890856e54d978958b217388adaedc46910a
Fogel Egor <fogel_92@bk.ru>
2022-07-12 22:15:59 +0700

    feat: enable drag and drop function

--- scripts/main.js
+++ scripts/main.js
@@ -45,6 +45,8 @@ function addTask() {
     value = '';
     form.style.display = 'none';
     btn.style.display = 'flex';
+
+    dragNdrop();
   });
 }
 
@@ -61,6 +63,7 @@ function addBoard() {
   boards.append(board);
 
   changeTitle();
+  dragNdrop();
 }
 
 button.addEventListener('click', addBoard);
@@ -73,3 +76,53 @@ function changeTitle() {
 }
 
 changeTitle();
+
+let draggedItem = null;
+
+function dragNdrop() {
+  const listItems = document.querySelectorAll('.list__item');
+  const lists = document.querySelectorAll('.list');
+
+  for (let i = 0; i < listItems.length; i++) {
+    const item = listItems[i];
+    item.addEventListener('dragstart', () => {
+      draggedItem = item;
+      setTimeout(() => {
+        item.style.display = 'none';
+      }, 0);
+    });
+
+    item.addEventListener('dragend', () => {
+      setTimeout(() => {
+        item.style.display = 'block';
+        draggedItem = null;
+      }, 0);
+    });
+
+    item.addEventListener('dblclick', () => {
+      item.remove();
+    });
+
+    for (let j = 0; j < lists.length; j++) {
+      const list = lists[j];
+
+      list.addEventListener('dragover', (evt) => evt.preventDefault());
+
+      list.addEventListener('dragenter', function (evt) {
+        evt.preventDefault();
+        this.style.backgroundColor = 'rgba(0,0,0,.3)';
+      });
+
+      list.addEventListener('dragleave', function () {
+        this.style.backgroundColor = 'rgba(0,0,0,0)';
+      });
+
+      list.addEventListener('drop', function () {
+        this.style.backgroundColor = 'rgba(0,0,0,0)';
+        this.append(draggedItem);
+      });
+    }
+  }
+}
+
+dragNdrop();
