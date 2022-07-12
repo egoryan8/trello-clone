2e4c517eb9c0a4be84764be4545699e270be2899
Fogel Egor <fogel_92@bk.ru>
2022-07-12 21:14:59 +0700

    feat: add style

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const lists = document.querySelectorAll('.list');
+
+function addTask() {
+  const btn = document.querySelector('.add__btn');
+  const addBtn = document.querySelector('.add__item-btn');
+  const cancelBtn = document.querySelector('.cancel__item-btn');
+  const textarea = document.querySelector('.textarea');
+  const form = document.querySelector('.form');
+}
